f16a42b603dde164010ed4ec5c1f45daef31a6da
Muhammed <Muhammed@ATLAS-H161UDRSF>
2022-11-05 06:12:16 +0300

    Window Events

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,20 +3,29 @@
 #include <GLFW/glfw3.h>
 
 #include "Application.h"
-#include "Hazel/Events/ApplicationEvent.h"
-
-
 
 namespace Hazel {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}",e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
@@ -25,5 +34,11 @@ namespace Hazel {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,7 +2,10 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
 #include "Window.h"
+
 
 namespace Hazel {
 
@@ -11,9 +14,13 @@ namespace Hazel {
 	public:
 		Application();
 		virtual ~Application();
-		void Run();
 
+		void Run();
+		
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
